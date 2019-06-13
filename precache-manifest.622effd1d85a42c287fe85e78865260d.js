ba396a237a16b2da328fa33ae1e9bb9d59825848
unknown <illya@plasson.co.il>
2019-06-14 02:34:38 +0300

    Updates

---
+++ precache-manifest.622effd1d85a42c287fe85e78865260d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c4391ba426a5c217fdde3b35802d60e",
+    "url": "/slideshow/index.html"
+  },
+  {
+    "revision": "8104aef06b584bc919c0",
+    "url": "/slideshow/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/slideshow/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "8104aef06b584bc919c0",
+    "url": "/slideshow/static/js/main.173d741e.chunk.js"
+  },
+  {
+    "revision": "580c07abd9b46abe6483",
+    "url": "/slideshow/static/js/runtime~main.2bc53cdc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/slideshow/static/media/logo.5d5d9eef.svg"
+  }
+]);
